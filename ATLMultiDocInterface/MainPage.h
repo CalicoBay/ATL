63a956a7700c5ec4e2655230edd215b4ce0d8bf8
Keesje at Telus <keesje@telus.net>
2024-07-06 14:43:51 -0700

    Added the MainPage group of files.

---
+++ ATLMultiDocInterface/MainPage.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::ATLMDI::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage() 
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::ATLMDI::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
