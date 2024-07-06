22b047af643cd7780b67469d82cb7ae431bcf53c
Keesje at Telus <keesje@telus.net>
2024-07-06 15:14:03 -0700

    Still running and adding more stuff.

--- ATLMultiDocInterface/ATL-MDI.cpp
+++ ATLMultiDocInterface/ATL-MDI.cpp
@@ -4,6 +4,8 @@
 #include "pch.h"
 #include "ATL-MDIChild.h"
 #include "ATL-MDI.h"
+#include <App.xaml.h>
+#include <MainPage.h>
 #include <Microsoft.UI.Dispatching.Interop.h> // For ContentPreTranslateMessage
 
 namespace winrt
@@ -15,11 +17,65 @@ namespace winrt
 	using namespace winrt::Microsoft::UI::Xaml::Markup;
 }
 
+//bool ProcessMessageForTabNavigation(const HWND topLevelWindow, MSG* msg);
+
+// Extra state for our top-level window, we point to from GWLP_USERDATA.
+struct WindowInfo
+{
+	winrt::DesktopWindowXamlSource DesktopWindowXamlSource{ nullptr };
+	winrt::event_token TakeFocusRequestedToken{};
+	HWND LastFocusedWindow{ NULL };
+};
+
+// Returns "true" if the function handled the message and it shouldn't be processed any further.
+// Intended to be called from the main message loop.
+bool ProcessMessageForTabNavigation(const HWND topLevelWindow, MSG* msg)
+{
+	if(msg->message == WM_KEYDOWN && msg->wParam == VK_TAB)
+	{
+		// The user is pressing the "tab" key.  We want to handle this ourselves so we can pass information into Xaml
+		// about the tab navigation.  Specifically, we need to tell Xaml whether this is a forward tab, or a backward
+		// shift+tab, so Xaml will know whether to put focus on the first Xaml element in the island or the last
+		// Xaml element.  (This is done in the call to DesktopWindowXamlSource.NavigateFocus()).
+		const HWND currentFocusedWindow = ::GetFocus();
+		if(::GetAncestor(currentFocusedWindow, GA_ROOT) != topLevelWindow)
+		{
+			// This is a window outside of our top-level window, let the system process it.
+			return false;
+		}
+
+		const bool isShiftKeyDown = ((HIWORD(::GetKeyState(VK_SHIFT)) & 0x8000) != 0);
+		const HWND nextFocusedWindow = ::GetNextDlgTabItem(topLevelWindow, currentFocusedWindow, isShiftKeyDown /*bPrevious*/);
+
+		WindowInfo* windowInfo = reinterpret_cast<WindowInfo*>(::GetWindowLongPtr(topLevelWindow, GWLP_USERDATA));
+		const HWND dwxsWindow = winrt::GetWindowFromWindowId(windowInfo->DesktopWindowXamlSource.SiteBridge().WindowId());
+		if(dwxsWindow == nextFocusedWindow)
+		{
+			// Focus is moving to our DesktopWindowXamlSource.  Instead of just calling SetFocus on it, we call NavigateFocus(),
+			// which allows us to tell Xaml which direction the keyboard focus is moving.
+			// If your app has multiple DesktopWindowXamlSources in the window, you'll want to loop over them and check to
+			// see if focus is moving to each one.
+			winrt::XamlSourceFocusNavigationRequest request{
+				 isShiftKeyDown ?
+					  winrt::XamlSourceFocusNavigationReason::Last :
+					  winrt::XamlSourceFocusNavigationReason::First };
+
+			windowInfo->DesktopWindowXamlSource.NavigateFocus(request);
+			return true;
+		}
+
+		// Focus isn't moving to our DesktopWindowXamlSource.  IsDialogMessage will automatically do the tab navigation
+		// for us for this msg.
+		const bool handled = (::IsDialogMessage(topLevelWindow, msg) == TRUE);
+		return handled;
+	}
+	return false;
+}
 
 LRESULT CATLMDIFrame::OnAboutBox(HWND hWnd, WORD , WORD , HWND , BOOL& )
 {
 	CString sTitle;
-	sTitle.LoadStringW(IDS_PROJNAME);
+	(void)sTitle.LoadStringW(IDS_PROJNAME);
     ::ShellAbout(hWnd, sTitle, NULL, NULL);
 	return 0;
 }
@@ -301,19 +357,37 @@ void CATLMDIModule::RunMessageLoop() throw()
 	try
 	{
 		auto dispatcherQueueController{ winrt::DispatcherQueueController::CreateOnCurrentThread() };
+		// Island-support: Create our custom Xaml App object. This is needed to properly use the controls and metadata
+		// in Microsoft.ui.xaml.controls.dll.
+		auto simpleIslandApp{ winrt::make<winrt::ATLMDI::implementation::App>() };
+
 		MSG msg = {};
 
 		while((GetMessage(&msg, 0, 0, 0) > 0))//WM_QUIT != msg.message
 		{
 			if(!::TranslateMDISysAccel(m_pFrame->m_hMDIClient, &msg))
 			{
-				if(0 == ::TranslateAccelerator(m_pFrame->m_hWnd, m_hAccelTable, &msg))
+				if(::ContentPreTranslateMessage(&msg))
 				{
-					::TranslateMessage(&msg);
-					::DispatchMessage(&msg);
+					continue;
 				}
+				if(::TranslateAccelerator(m_pFrame->m_hWnd, m_hAccelTable, &msg))
+				{
+					continue;
+				}
+
+				// Island-support: This is needed so that the user can correctly tab and shift+tab into islands.
+				if(ProcessMessageForTabNavigation(m_pFrame->m_hWnd, &msg))
+				{
+					continue;
+				}
+
+				::TranslateMessage(&msg);
+				::DispatchMessage(&msg);
 			}
 		}
+
+		// Island-support: To properly shut down after using a DispatcherQueue, call ShutdownQueue[Aysnc]().
 		dispatcherQueueController.ShutdownQueue();
 	}
 	catch(const winrt::hresult_error& exception)
