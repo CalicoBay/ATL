208cd463789f77a2cd74b85b9ea1b6f7f04292fd
Calicoba at Telus <calicoba@telus.net>
2024-07-13 16:51:33 -0700

    Tabs were out of wack.

--- ATLMultiDocInterface/MainPage.cpp
+++ ATLMultiDocInterface/MainPage.cpp
@@ -6,81 +6,81 @@
 
 namespace winrt
 {
-	using namespace winrt::Microsoft::UI;
-	using namespace winrt::Microsoft::UI::Dispatching;
-	using namespace winrt::Microsoft::UI::Xaml;
+   using namespace winrt::Microsoft::UI;
+   using namespace winrt::Microsoft::UI::Dispatching;
+   using namespace winrt::Microsoft::UI::Xaml;
    using namespace winrt::Microsoft::UI::Xaml::Controls;
    using namespace winrt::Microsoft::UI::Xaml::Hosting;
-	using namespace winrt::Microsoft::UI::Xaml::Markup;
+   using namespace winrt::Microsoft::UI::Xaml::Markup;
    using namespace winrt::Windows::Foundation;
 }
 
 namespace winrt::ATLMDI::implementation
 {
-    int32_t MainPage::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
+   int32_t MainPage::MyProperty()
+   {
+      throw hresult_not_implemented();
+   }
 
-    void MainPage::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
+   void MainPage::MyProperty(int32_t /* value */)
+   {
+      throw hresult_not_implemented();
+   }
 
-    Uri MainPage::Source()
-    {
-       return OurWeb().Source();
-    }
+   Uri MainPage::Source()
+   {
+      return OurWeb().Source();
+   }
 
-    void MainPage::Source(Uri const& value)
-    {
-       OurWeb().Source(value);
-    }
+   void MainPage::Source(Uri const& value)
+   {
+      OurWeb().Source(value);
+   }
 
-    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
-    {
+   void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+   {
       try
       {
          Uri targetUri(addressBar().Text());
          OurWeb().Source(targetUri);
       }
-      catch(const winrt::hresult_error& exception)
+      catch (const winrt::hresult_error& exception)
       {
          CString sTryAgain;
          sTryAgain.Format(_T("%s Try again"), exception.message().c_str());
          addressBar().Text((LPCTSTR)sTryAgain);
       }
-    }
+   }
 
-    void MainPage::NavStarting(IInspectable const& sender, Microsoft::Web::WebView2::Core::CoreWebView2NavigationStartingEventArgs const& args)
-    {
-       if(0 == m_pendingNavID)
-       {
-          m_pendingUri = args.Uri();
-          m_pendingNavID = args.NavigationId();
-       }
-       else
-       {
-          bool bSomethingIsPending = true;
-       }
-    }
+   void MainPage::NavStarting(IInspectable const& sender, Microsoft::Web::WebView2::Core::CoreWebView2NavigationStartingEventArgs const& args)
+   {
+      if (0 == m_pendingNavID)
+      {
+         m_pendingUri = args.Uri();
+         m_pendingNavID = args.NavigationId();
+      }
+      else
+      {
+         bool bSomethingIsPending = true;
+      }
+   }
 
-    void MainPage::NavCompleted(Windows::Foundation::IInspectable const& sender, Microsoft::Web::WebView2::Core::CoreWebView2NavigationCompletedEventArgs const& args)
-    {
-       if(args.IsSuccess())
-       {
-          if(args.NavigationId() == m_pendingNavID)
-          {
-             addressBar().Text(m_pendingUri.c_str());
-             m_pendingNavID = 0;
-          }
-       }
-       else
-       {
-          bool bNavFailed = true;
-          m_pendingNavID = 0;
-          int32_t error = args.HttpStatusCode();
-          m_pendingUri.clear();
-       }
-    }
+   void MainPage::NavCompleted(Windows::Foundation::IInspectable const& sender, Microsoft::Web::WebView2::Core::CoreWebView2NavigationCompletedEventArgs const& args)
+   {
+      if (args.IsSuccess())
+      {
+         if (args.NavigationId() == m_pendingNavID)
+         {
+            addressBar().Text(m_pendingUri.c_str());
+            m_pendingNavID = 0;
+         }
+      }
+      else
+      {
+         bool bNavFailed = true;
+         m_pendingNavID = 0;
+         int32_t error = args.HttpStatusCode();
+         m_pendingUri.clear();
+      }
+   }
 }
--- ATLMultiDocInterface/MainPage.h
+++ ATLMultiDocInterface/MainPage.h
@@ -6,30 +6,30 @@
 
 namespace winrt::ATLMDI::implementation
 {
-    struct MainPage : MainPageT<MainPage>
-    {
-       winrt::hstring m_pendingUri;
-       uint64_t m_pendingNavID;
-       MainPage() : m_pendingNavID(0)
-        {
-            // Xaml objects should not call InitializeComponent during construction.
-            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
-        }
+   struct MainPage : MainPageT<MainPage>
+   {
+      winrt::hstring m_pendingUri;
+      uint64_t m_pendingNavID;
+      MainPage() : m_pendingNavID(0)
+      {
+         // Xaml objects should not call InitializeComponent during construction.
+         // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+      }
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-        Windows::Foundation::Uri Source();
-        void Source(Windows::Foundation::Uri const& value);
+      int32_t MyProperty();
+      void MyProperty(int32_t value);
+      Windows::Foundation::Uri Source();
+      void Source(Windows::Foundation::Uri const& value);
 
-        void ClickHandler(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
-        void NavStarting(Windows::Foundation::IInspectable const& sender, Microsoft::Web::WebView2::Core::CoreWebView2NavigationStartingEventArgs const& args);
-        void NavCompleted(Windows::Foundation::IInspectable const& sender, Microsoft::Web::WebView2::Core::CoreWebView2NavigationCompletedEventArgs const& args);
-    };
+      void ClickHandler(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+      void NavStarting(Windows::Foundation::IInspectable const& sender, Microsoft::Web::WebView2::Core::CoreWebView2NavigationStartingEventArgs const& args);
+      void NavCompleted(Windows::Foundation::IInspectable const& sender, Microsoft::Web::WebView2::Core::CoreWebView2NavigationCompletedEventArgs const& args);
+   };
 }
 
 namespace winrt::ATLMDI::factory_implementation
 {
-    struct MainPage : MainPageT<MainPage, implementation::MainPage>
-    {
-    };
+   struct MainPage : MainPageT<MainPage, implementation::MainPage>
+   {
+   };
 }
